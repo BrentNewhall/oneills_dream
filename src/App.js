37a09281c9e94ea560bfe83b8b0cd4624e0227fc
Brent Newhall <brent@brentnewhall.com>
2018-12-31 11:07:48 -0500

    Added collector ship.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 class App extends Component {
   constructor( props ) {
     super( props );
+    // Create asteroids
     this.asteroids = [];
     for( let i = 0; i < 10; i++ ) {
       const asteroid = {
@@ -13,9 +14,16 @@ class App extends Component {
       }
       this.asteroids.push( asteroid );
     }
+    // Create collectors
+    this.collectors = [];
+    this.collectors.push({
+      x: 250,
+      y: 250
+    });
   }
 
   render() {
+    // Set up asteroid images
     const asteroidObjects = this.asteroids.map( (asteroid, index) => {
       const asteroidStyle = {
         left: asteroid.x,
@@ -26,10 +34,21 @@ class App extends Component {
       return <img style={asteroidStyle} alt={'Asteroid ' + index}
           key={'asteroid' + index} className='asteroid'
           src='images/asteroid.png' />
-    })
+    });
+    // Set up collector images
+    const collectorObjects = this.collectors.map( (collector, index) => {
+      const collectorStyle = {
+        left: collector.x,
+        top: collector.y
+      }
+      return <img style={collectorStyle} alt={'Collector ' + index}
+          key={'collector' + index} className='collector'
+          src='images/collector.png' />
+    });
     return (
       <div className="Space">
         {asteroidObjects}
+        {collectorObjects}
       </div>
     );
   }
