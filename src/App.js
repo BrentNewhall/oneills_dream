c313b6062a081be5118b15e580f62354b043474e
Brent Newhall <brent@brentnewhall.com>
2018-12-31 11:03:17 -0500

    Initial creation of repository with basic setup.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor( props ) {
+    super( props );
+    this.asteroids = [];
+    for( let i = 0; i < 10; i++ ) {
+      const asteroid = {
+        x: Math.random() * 500,
+        y: Math.random() * 500,
+        size: Math.random() * 30 + 30
+      }
+      this.asteroids.push( asteroid );
+    }
+  }
+
   render() {
+    const asteroidObjects = this.asteroids.map( (asteroid, index) => {
+      const asteroidStyle = {
+        left: asteroid.x,
+        top: asteroid.y,
+        height: asteroid.size,
+        width: asteroid.size
+      }
+      return <img style={asteroidStyle} alt={'Asteroid ' + index}
+          key={'asteroid' + index} className='asteroid'
+          src='images/asteroid.png' />
+    })
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Space">
+        {asteroidObjects}
       </div>
     );
   }
