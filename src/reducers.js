429a24a97f29ff4d49451db13ca719fd2a825cff
Brent Newhall <brent@brentnewhall.com>
2019-01-01 15:13:41 -0500

    Fixed reference to amount of aluminum in data.

--- src/reducers.js
+++ src/reducers.js
@@ -3,10 +3,10 @@ import ACTION_ADD_ALUMINUM from './global';
 function reducers( state, action ) {
     switch( action.type ) {
         case ACTION_ADD_ALUMINUM:
-            console.log( "Aluminimum added!" );
+            console.log( action.data.amount + " aluminimum added!" );
             return {
                 ...state,
-                aluminum: action.amount
+                aluminum: action.data.amount
             };
         default:
             return state;
