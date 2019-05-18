254ef8c4b3e8aea0e42c1baa1564ed1b8b30a3ac
Brent P. Newhall <brent@brentnewhall.com>
2019-05-18 03:17:29 -0400

    Add CombatShip and export to Ship

---
+++ src/CombatShip.js
@@ -0,0 +1,59 @@
+import Ship from './Ship';
+
+class CombatShip extends Ship {
+    constructor( x, y, width, height ) {
+        super();
+        this.setDimensions( x, y, width, height );
+        this.attackCountdown = 0;
+        this.attackCountdownMax = 50;
+        this.attackPower = 10;
+        this.attackRange = 80;
+    }
+
+    /* Returns target if it was hit by an attack; null otherwise. */
+    attack() {
+        const originalTarget = this.target;
+        if( this.target !== null  && this.distance(this.target) <= this.attackRange ) {
+            if( this.attackCountdown > 0 ) {
+                this.attackCountdown -= 1;
+            }
+            else {
+                this.attackCountdown = this.attackCountdownMax;
+                this.target.armor -= this.attackPower;
+                if( this.target.armor <= 0 ) {
+                    this.target = null;
+                }
+                return originalTarget;
+            }
+        }
+        return null;
+    }
+
+    _findNearestTarget( targets ) {
+        if( targets === null  ||  targets.length === 0 ) {
+            return null;
+        }
+        const here = this;
+        let minDistance = 1000000000;
+        let nearestTarget = null;
+        targets.forEach( (target) => {
+            const d = here.distance(target);
+            if( d < minDistance ) {
+                minDistance = d;
+                nearestTarget = target;
+            }
+        });
+        return nearestTarget;
+    }
+
+    findTarget( targets ) {
+        if( this.target === null ) {
+            const nearestTarget = this._findNearestTarget( targets );
+            if( nearestTarget !== null ) {
+                this.target = nearestTarget;
+            }
+        }
+    }
+}
+
+export default CombatShip;
--- src/Ship.js
+++ src/Ship.js
@@ -54,3 +54,5 @@ class Ship {
         }
     }
 }
+
+export default Ship;
